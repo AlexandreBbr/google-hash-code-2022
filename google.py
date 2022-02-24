8009538ede3513322a710aefa775f3000ae16ec2
Skywear <alexandre.barbier@epitech.eu>
2022-02-24 19:59:18 +0100

    [ADD] First exercice solved (33 points added)

--- google.py
+++ google.py
@@ -4,13 +4,13 @@ def main():
     f=open("a_example.txt", "w+")
 
     for i in range(1):
-        f.write("3 \tthree projects are planned\n")
-        f.write("WebServer \tassignments for project WebServer\n")
-        f.write("Bob Anna \tBob -> first role, Anna -> second role\n")
-        f.write("Logging \tassignments for project Logging\n")
-        f.write("Anna \t\tAnna -> first role\n")
-        f.write("WebChat \tassignments for project WebChat\n")
-        f.write("Maria Bob \tMaria -> first role, Bob -> second role\n")
+        f.write("3\n")
+        f.write("WebServer\n")
+        f.write("Bob Anna\n")
+        f.write("Logging\n")
+        f.write("Anna\n")
+        f.write("WebChat\n")
+        f.write("Maria Bob\n")
     f.close()
 
 if __name__== "__main__":
